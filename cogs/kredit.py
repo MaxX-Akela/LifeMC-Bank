66c07fed5f6f7e50e838ac13cde65ce2c89d0a3e
MaxX_Akela <145983198+MaxX-Akela@users.noreply.github.com>
2025-01-18 23:19:15 +0700

    Update kredit.py

--- cogs/kredit.py
+++ cogs/kredit.py
@@ -29,30 +29,30 @@ class CreditRating(commands.Cog):
 
     @commands.slash_command(description="Отображает кредитные рейтинги всех участников (без ботов).")
     async def credit_rating(self, inter):
-        
+
         members = [member for member in inter.guild.members if not member.bot]
-        
+
         embed = disnake.Embed(title="Кредитный рейтинг участников", color=disnake.Color.blue())
-        
+
         for member in members:
             credit_score = self.credit_scores.get(str(member.id), 100) 
             embed.add_field(name=member.name, value=f"Кредитный рейтинг: {credit_score}", inline=False)
-            
+
         embed.set_footer(text=f"Запросил: {inter.author.name}", icon_url=inter.author.avatar.url)
         await inter.send(embed=embed)
 
     @commands.slash_command(description="Добавляет кредитный рейтинг участнику.")
     async def add_credit(self, inter, member: disnake.Member = Option(name="member", description="Участник", required=True), amount: int = Option(name="amount", description="Количество, на которое увеличим рейтинг", required=True)):
-        
+
         if member.bot:
             await inter.send("Ботам нельзя добавлять кредитный рейтинг.")
             return
 
 
-        current_score = self.credit_scores.get(str(member.id), 700) 
+        current_score = self.credit_scores.get(str(member.id), 100) 
         new_score = current_score + amount
         self.credit_scores[str(member.id)] = new_score
-        
+
 
         self.save_credit_scores()
 
@@ -64,21 +64,21 @@ class CreditRating(commands.Cog):
         )
         embed.add_field(name="Новый кредитный рейтинг:", value=f"**{new_score}**", inline=False)
         embed.set_footer(text=f"Запросил: {inter.author.name}", icon_url=inter.author.avatar.url)
-        
+
         await inter.send(embed=embed)
 
     @commands.slash_command(description="Снимает кредитный рейтинг с участника.")
     async def remove_credit(self, inter, member: disnake.Member = Option(name="member", description="Участник", required=True), amount: int = Option(name="amount", description="Количество, на которое уменьшим рейтинг", required=True)):
-        
+
         if member.bot:
             await inter.send("Ботам нельзя снимать кредитный рейтинг.")
             return
 
- 
+
         current_score = self.credit_scores.get(str(member.id), 100)  
         new_score = max(0, current_score - amount)  
         self.credit_scores[str(member.id)] = new_score
-        
+
 
         self.save_credit_scores()
 
@@ -90,16 +90,16 @@ class CreditRating(commands.Cog):
         )
         embed.add_field(name="Новый кредитный рейтинг:", value=f"**{new_score}**", inline=False)
         embed.set_footer(text=f"Запросил: {inter.author.name}", icon_url=inter.author.avatar.url)
-        
+
         await inter.send(embed=embed)
 
     @commands.slash_command(description="Запрашивает кредитный рейтинг участника.")
     async def query_credit(self, inter, member: disnake.Member = Option(name="member", description="Участник", required=True)):
 
-        
 
-        credit_score = self.credit_scores.get(str(member.id), 700) 
-        
+
+        credit_score = self.credit_scores.get(str(member.id), 100) 
+
 
         embed = disnake.Embed(
             title=f"Кредитный рейтинг {member.name}",
@@ -107,7 +107,7 @@ class CreditRating(commands.Cog):
             color=disnake.Color.blue()
         )
         embed.set_footer(text=f"Запросил: {inter.author.name}", icon_url=inter.author.avatar.url)
-        
+
         await inter.send(embed=embed)
 
 
